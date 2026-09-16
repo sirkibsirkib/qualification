1a0803e15f32ae6d7b76c4d09fe0e42fd64847eb
Christopher Esterhuyse <christopher.esterhuyse@gmail.com>
2026-09-16 11:00:34 +0200

    sync

--- Qualification/Basic.lean
+++ Qualification/Basic.lean
@@ -6,6 +6,28 @@ structure Lts: Type 1 where mk::
   Label: Type
   trans: State → Label → State → Prop
 
+def lts1: Lts := {
+  State := Nat
+  initial := 0
+  Label := String
+  trans s1 l s2 := s2 = s1+1 ∧ l = "inc"
+}
+
+
+def lts2: Lts := {
+  State := String
+  initial := "init"
+  Label := String
+  trans _ l _ := l = "inc"
+}
+
+notation lts " # " s1 " —" l "⟶ " s2 => Lts.trans lts s1 l s2
+
+example: (lts1: Lts) # (6: Nat) — "inc" ⟶ (7: Nat) := sorry
+
+example: lts2 # "init" —"inc"⟶ "cool" :=
+  sorry
+
 structure Model where mk::
   Fluent: Type
   lts: Lts
@@ -24,3 +46,10 @@ def ConModel' (Constant: Type) :=
   }
 
 example: Type 1 := ConModel' String
+
+def Lts.trans_clos (lts: Lts)
+: lts.State → List lts.Label → lts.State → Prop :=
+  ReflTransLab lts.trans
+
+
+notation lts "# " s1 "—" l "⟶⋆" s2 => Lts.trans_clos lts s1 l s2
--- Qualification/Utils.lean
+++ Qualification/Utils.lean
@@ -1,5 +1,6 @@
 
-abbrev EndoRel α := α → α → Prop
+abbrev Rel α β := α → β → Prop
+abbrev EndoRel α := Rel α α
 
 inductive ReflTransGen {α} (r: EndoRel α): EndoRel α where
   | refl (x: α): ReflTransGen r x x
@@ -15,3 +16,19 @@ example: ReflTransGen all_nat 0 5 := by
   . exact ReflTransGen.refl 0
   . unfold all_nat
     exact True.intro
+
+
+inductive ReflTransLab {State Label: Type} (r: State → Label → State → Prop)
+: State → List Label → State → Prop where
+  | refl (s: State): ReflTransLab r s [] s
+  | cons (s1 s2 s3: State) (l12: Label) (l23: List Label):
+                   r s1 l12 s2 →
+      ReflTransLab r s2 l23 s3 →
+      ReflTransLab r s1 (l12 :: l23) s3
+
+theorem List.snoc {State Label: Type} {r: State → Label → State → Prop}:
+  ∀ (s1 s2 s3: State) (l12: List Label) (l23: Label),
+      ReflTransLab r s1 l12 s2 →
+                   r s2 l23 s3 →
+      ReflTransLab r s1 (l23 :: l12) s3
+:= by sorry
